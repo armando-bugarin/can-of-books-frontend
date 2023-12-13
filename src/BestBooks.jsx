6f116b9335a1fa701e97e3b3505e3025f0d2d000
Charlie Arriaga <arriayaa@gmail.com>
2023-12-12 17:46:16 -0800

    rendered books

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,31 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel'
 
-class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
-  }
+function BestBooks() {
+  // constructor (props) {
+  //   super(props);
+  //   this.state = {
+  //     books: []
+  //   }
+  // }
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    mount();
+  }, []);
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
-  render() {
+  async function mount() {
+    try {
+      // app.get('/books', async (request, response) => {
+      const response = await axios.get('http://localhost:3000/books');
+      if (response.data) {
+        console.log(response.data)
+        setBooks(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
 
-    /* TODO: render all the books in a Carousel */
-
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    )
   }
+
+  // Mount()
+
+
+
+  return (
+    <>
+      <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+
+      {books.length ? (
+        <Carousel>
+          <Carousel.Item>
+            <h3>{books[0].title}</h3>
+            <p>{books[0].description}</p>
+            <p>Status: {books[0].status}</p>
+          </Carousel.Item>
+          <Carousel.Item>
+          <h3>{books[3].title}</h3>
+            <p>{books[3].description}</p>
+            <p>Status: {books[3].status}</p>
+          </Carousel.Item>
+          <Carousel.Item>
+          <h3>{books[4].title}</h3>
+            <p>{books[4].description}</p>
+            <p>Status: {books[4].status}</p>
+          </Carousel.Item>
+        </Carousel>
+      ) : (
+        <Carousel.Item>
+          <h3>No Books Found </h3>
+        </Carousel.Item >
+      )}
+    </>
+  )
 }
 
 export default BestBooks;
